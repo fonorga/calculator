cfdcc1dceccc294dabe479ffe789e283ea6dd259
Alfonso Orviz Gago <orviz.fonso@gmail.com>
2023-06-23 11:23:01 +0200

    Mejoras en la implementación de Swagger2

--- src/main/java/com/ciber/calculator/controller/CalculatorController.java
+++ src/main/java/com/ciber/calculator/controller/CalculatorController.java
@@ -1,5 +1,9 @@
 package com.ciber.calculator.controller;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.util.List;
 
 import javax.validation.constraints.NotEmpty;
@@ -19,6 +23,7 @@ import com.ciber.calculator.service.Calculator;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import springfox.documentation.builders.PathSelectors;
@@ -27,46 +32,45 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @RestController
-@Api(tags = "Calculator API")
+@Api(tags = "Calculator API", description = "Incluye varios métodos para realizar operaciones con listas de números")
 class CalculatorController {
     @Autowired
     private Calculator calculator;
 
-    @ApiOperation(value = "Sumar números pares", response =  Calculator.class)
+    //Anotación personalizada para evitar repetir el código para cada método
+    @Target(ElementType.METHOD)
+    @Retention(RetentionPolicy.RUNTIME)
     @ApiResponses(value = {
         @ApiResponse(code = 200, message = "OK"),
         @ApiResponse(code = 400, message = "Solicitud inválida"),
+        @ApiResponse(code = 500, message = "Error interno del servidor")
     })
+    public @interface ApiResponsesForStringResponse {
+    }
 
     //Método para sumar los números pares de una lista
     @PostMapping("/sumEvenNumbers")
-    ResponseEntity<String> sumEvenNumbers(@RequestParam("numbers") @NotEmpty List<Integer> numList){
+    @ApiOperation(value = "Sumar números pares", response = ResponseEntity.class)
+    @ApiResponsesForStringResponse
+    ResponseEntity<String> sumEvenNumbers(@ApiParam(value = "Lista de números", required = true) @RequestParam("numbers") @NotEmpty List<Integer> numList){
         int sum = calculator.sumEvenNumbers(numList); //Se llama al servicio. La lista se pasó como parámetro en la URL
         return ResponseEntity.ok("La suma de números pares es: " + sum); //Devuelve el código HTTP 200 OK
     }
-
-    @ApiOperation(value = "Multiplicar números impares", response =  Calculator.class)
-    @ApiResponses(value = {
-        @ApiResponse(code = 200, message = "OK"),
-        @ApiResponse(code = 400, message = "Solicitud inválida"),
-    })
-
+    
     //Método para multiplicar los números impares de una lista
     @PostMapping("/multiplyOddNumbers")
-    ResponseEntity<String> multiplyOddNumbers(@RequestParam("numbers") @NotEmpty List<Integer> numList){
+    @ApiOperation(value = "Multiplicar números impares", response = ResponseEntity.class)
+    @ApiResponsesForStringResponse
+    ResponseEntity<String> multiplyOddNumbers(@ApiParam(value = "Lista de números", required = true) @RequestParam("numbers") @NotEmpty List<Integer> numList){
         int mult = calculator.multiplyOddNumbers(numList); //Se llama al servicio. La lista se pasó como parámetro en la URL
         return ResponseEntity.ok("El producto de los números impares es: " + mult); //Devuelve el código HTTP 200 OK
     }
-
-    @ApiOperation(value = "Identificar números primos", response =  Calculator.class)
-    @ApiResponses(value = {
-        @ApiResponse(code = 200, message = "OK"),
-        @ApiResponse(code = 400, message = "Solicitud inválida"),
-    })
-
+    
     //Método para contar los números primos de una lista y devolver la lista marcando los que lo son con un asterisco
     @PostMapping("/primeNumbers")
-    ResponseEntity<String> primeNumbers(@RequestParam("numbers") @NotEmpty List<Integer> numList){
+    @ApiOperation(value = "Identificar números primos", response = ResponseEntity.class)
+    @ApiResponsesForStringResponse
+    ResponseEntity<String> primeNumbers(@ApiParam(value = "Lista de números", required = true) @RequestParam("numbers") @NotEmpty List<Integer> numList){
         List<String> result = calculator.primeNumbers(numList); //Se llama al servicio. La lista se pasó como parámetro en la URL
         return ResponseEntity.ok("Lista de números (primos con asterisco): " + result); //Devuelve el código HTTP 200 OK
     }
